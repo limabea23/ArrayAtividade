629c9a1b659eceac0cbaa40b0facfd9268b5350b
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 11:13:09 -0300

    método3: reverse = Inverte a ordem dos elementos de um array.

---
+++ reverse.js
@@ -0,0 +1,11 @@
+//método3: reverse = Inverte a ordem dos elementos de um array.
+
+//exemplo 1
+let array = [5, 10, 15, 20, 25, 30]; //declara a variavél
+array.reverse(); //inverte a ordem
+console.log(array); //exibe a mensagem (resultado)
+
+//exemplo 2
+let elementos = ["ar", "fogo", "terra", "água"]; //declara a variavél
+elementos.reverse(); //inverte a ordem
+console.log(elementos); //exibe a mensagem (resultado)
