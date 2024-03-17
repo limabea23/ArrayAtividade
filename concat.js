7602567e10952f5c650e679e9dad91f26babcd23
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 11:53:16 -0300

    método5: concat = Junta (concatena) dois ou mais arrays, retornando um novo array que contém todos os elementos dos arrays originais combinados em um único array.

---
+++ concat.js
@@ -0,0 +1,13 @@
+//método5: concat = Junta (concatena) dois ou mais arrays, retornando um novo array que contém todos os elementos dos arrays originais combinados em um único array.
+
+//exemplo 1
+let array1 = [9, 18, 27]; //declara a variavél
+let array2 = [3, 6, 9]; //declara outra variavél
+let concatenatedArray = array1.concat(array2); //cria um array que junta todos os elementos do array1 e array2
+console.log(concatenatedArray); 
+
+//exemplo 2
+let letras = ["a", "b", "c"]; //declara a variavél
+let letras2 = ["z", "y", "x"]; //declara a variavél
+let juntaArray = letras.concat(letras2); //cria um array que junta todos os elementos do letras e letras2
+console.log(juntaArray); //exibe a mensagem (resultado)
