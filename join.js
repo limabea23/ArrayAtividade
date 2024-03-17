8a48b90892266cec285ea9ef91ef5cb691a483ac
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 11:04:30 -0300

    método1: join = Inverte a ordem dos elementos de um array.

---
+++ join.js
@@ -0,0 +1,11 @@
+//método1: join = Inverte a ordem dos elementos de um array.
+
+//exemplo 1
+let músicas = ["Sing of the Times", "Lights Up", "Satellite", "Fine Line"]; //declara a variavél
+let resultado = músicas.join (", "); //inverte a ordem
+console.log(resultado); //exibe a mensagem (resultado)
+
+//exemplo 2
+let comidas = ["Hambúrguer", "Tacos", "Maionese", "Bife"]; //declara a variavél
+let final = comidas.join (", "); //inverte a ordem
+console.log(final); //exibe a mensagem (resultado)
