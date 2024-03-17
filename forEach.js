b779b41a1cffc434da9e7235e058510ee8844712
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 11:18:37 -0300

    método4: forEach = Executa uma função em cada elemento do array.

---
+++ forEach.js
@@ -0,0 +1,12 @@
+//método4: forEach = Executa uma função em cada elemento do array.
+
+let séries = ["On My Block", "Lupin", "iCarly", "Outer Banks"]; //declara a variavél
+séries.forEach(function(séries){ //executa a função
+    console.log(séries); //exibe a mensagem (resultado)
+});
+
+//exemplo 2
+let bebidas = ["Água", "Café", "Suco", "Refrigerante"]; //declara a variavél
+bebidas.forEach(function(bebidas){ //executa a função
+    console.log(bebidas); //exibe a mensagem (resultado)
+});
