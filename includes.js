1c0988e229fbb3bc3d27f9a03a4fb09595861306
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 17:06:30 -0300

    //método6: includes = Verifica se um array contém um determinado elemento, retornando true ou false conforme apropriado.

---
+++ includes.js
@@ -0,0 +1,13 @@
+//método6: includes = Verifica se um array contém um determinado elemento, retornando true ou false conforme apropriado.
+
+//exemplo 1
+let array = ["Samsung", "Apple", "Xiaomi", "Motorola"]; //declara a variavél
+console.log(array.includes("Apple")); //verifica se o elemento está presente no array e exibe a mensagem de verdadeiro ou falso (neste caso, verdadeiro)
+console.log(array.includes("Poco")); //verifica se o elemento está presente no array e exibe a mensagem de verdadeiro ou falso (neste caso, falso)
+
+//exemplo 2
+let fastFood = ["Mc Donalds", "KFC", "Burguer King", "Taco Bell"]; //declara a variavél
+console.log(fastFood.includes("Subway")); //verifica se o elemento está presente no array e exibe a mensagem de verdadeiro ou falso (neste caso, falso)
+console.log(fastFood.includes("Taco Bell")); //verifica se o elemento está presente no array e exibe a mensagem de verdadeiro ou falso (neste caso, verdadeiro)
+
+
