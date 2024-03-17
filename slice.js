3b08d6ac922417ea78f970d5463eaf6f4ab83ad9
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 18:12:38 -0300

    método9: slice = Retorna uma cópia superficial de uma porção do array em um novo array.

---
+++ slice.js
@@ -0,0 +1,11 @@
+//método9: slice = Retorna uma cópia superficial de uma porção do array em um novo array.
+
+//exemplo 1
+let países = ["Brasil", "México", "Autrália", "Coreia do Sul"]; //declara a variável
+let slicedPaíses = países.slice(2, 3); //retorna uma cópia
+console.log(slicedPaíses); //exibe a mensagem (resultado)
+
+//exemplo 2
+let gírias = ["OMG", "ASAP", "BRB", "LMK"]; //declara a variável
+let slicedGírias = gírias.slice(1, 3); //retorna uma cópia
+console.log(slicedGírias); //exibe a mensagem (resultado)
