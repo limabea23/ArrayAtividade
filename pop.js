adadf23acd31c3b6135bc682ae17b9fd6d572d22
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 17:26:34 -0300

    método7: pop = Remove o último elemento do array e o retorna.

---
+++ pop.js
@@ -0,0 +1,12 @@
+//método7: pop = Remove o último elemento do array e o retorna.
+
+//exemplo 1
+let hérois = ["homem aranha", "pantera negra", "hulk"]; //declara a variável
+let removeHérois = hérois.pop(); //remove o último elemento do array
+console.log(hérois); //exibe a mensagem (resultado)
+
+//exemplo 2
+let princesas = ["Tiana", "Cinderela", "Ariel"]; //declara a variável
+let removePrincesas = princesas.pop(); //remove o último elemento do array
+console.log(princesas); //exibe a mensagem (resultado)
+
