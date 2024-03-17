45420ddecac407fbc41e3a71d1885cb3a5eddda9
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 18:31:01 -0300

    //método10: shift.js =  Remove o primeiro elemento do array e o retorna.

---
+++ shift.js
@@ -0,0 +1,11 @@
+//método10: shift =  Remove o primeiro elemento do array e o retorna.
+
+//exemplo 1
+let playstation = ["PS1", "PS2", "PS3", "PS4", "PS5"]; //declara a variável
+let removePlaystation = playstation.shift(); //remove o primeiro elemento do array
+console.log(playstation); //exibe a mensagem (resultado)
+
+//exemplo 2
+let fifa = ["Fifa14", "Fifa16", "Fifa18", "Fifa20", "Fifa22"]; //declara a variável
+let removeFifa = fifa.shift(); //remove o primeiro elemento do array
+console.log(fifa); //exibe a mensagem (resultado)
