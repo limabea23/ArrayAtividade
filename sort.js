82d5ea387c62d3e109f71bc5a4cbc2188cbe1bf6
limabea23 <beatrizlima.tdsl.senai@gmail.com>
2024-03-17 11:08:03 -0300

    método2: sort = Ordena os elementos de um array (em ordem alfabética, por padrão) e retorna o array ordenado.

---
+++ sort.js
@@ -0,0 +1,11 @@
+//método2: sort = Ordena os elementos de um array (em ordem alfabética, por padrão) e retorna o array ordenado.
+
+//exemplo 1
+let app = ["snapchat", "vsco", "instagram", "tik tok", "whatsapp"]; //declara a variavél
+app.sort(); //ordena o array em ordem alfabética
+console.log(app); //exibe a mensagem (resultado)
+
+//exemplo 2
+let sistemasOperacionais = ["Windowns", "Linux", "MacOs", "Android"]; //declara a variavél
+sistemasOperacionais.sort(); //ordena o array em ordem alfabética
+console.log(sistemasOperacionais); //exibe a mensagem (resultado)
